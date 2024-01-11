5184ccc6bed1a3012d23b7690a7c83996c936819
sanju <sanjutiwari8827@gmail.com>
2024-01-11 19:58:29 +0530

    second message

--- src/MethodOverloading/Addition.java
+++ src/MethodOverloading/Addition.java
@@ -1,4 +1,44 @@
 package MethodOverloading;
 
 public class Addition {
+    public int add(int a ,int b ){
+
+        int c ;
+        c=a+b;
+        return c;
+    }
+    public int add(int a ,int b ,int c){
+
+        int d ;
+        d=a+b+c;
+        return d;
+    }
+    public int add(int a ,int b ,int c,int d){
+
+        int e ;
+        e=a+b+c+d;
+        return e;
+    }
+
+    public double add(double i, double j){
+        double k;
+        k=i+j;
+        return k;
+    }
+
+    public String add(String s1 , String s2){
+        String s3 ;
+        s3=s1+s2;
+        return s3;
+    }
+
+    public void show(int age,String name ){
+        System.out.println("age= "+age);
+        System.out.println("name= "+name);
+    }
+
+    public void show(String name, int age ){
+        System.out.println("name= "+name);
+        System.out.println("age= "+age);
+    }
 }
--- src/MethodOverloading/UseAddition.java
+++ src/MethodOverloading/UseAddition.java
@@ -1,4 +1,23 @@
 package MethodOverloading;
 
 public class UseAddition {
+
+    public static void main(String[] args) {
+        Addition obj =new Addition();
+        // type of parameter -1
+//        System.out.println("sum of 10 and 20 is "+obj.add(10,20));
+//        System.out.println("sum of 10.5 and 20.5 is "+obj.add(10.5,20.5));
+//        System.out.println("concetenation of Good and Morning is "+obj.add("Godd","Morning"));
+
+        // number of perameter........-2
+
+//        System.out.println("sum of 10 and 20 is "+obj.add(10,20));
+//        System.out.println("sum of 10,20and 30 is "+obj.add(10,20,30));
+//        System.out.println("sum of 10,20,30and 50 is "+obj.add(10,20,30,50));
+
+        //overloading on the basis of order of argument....
+        obj.show(21,"ajeet");
+        obj.show("ajeet",23);
+    }
+
 }
